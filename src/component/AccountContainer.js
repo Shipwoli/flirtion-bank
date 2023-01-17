8958b15d5a4a7d180f71d20534dd0d656875baa5
+ <+>
2023-01-17 15:06:25 +0300

    Resolve git conflicts

---
+++ src/component/AccountContainer.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import TransactionsList from "./TransactionList";
+import Search from "./Search";
+import AddTransactionForm from "./AddTransactionForm";
+
+function AccountContainer() {
+  const [transaction, setTransaction] = useState([])
+  const [query, setQuery] = useState("")
+  useEffect(() => {
+    fetch("https://api-test-7h6w.onrender.com/transactions/" + query)
+      .then((resp) => resp.json())
+      .then(transaction => setTransaction(transaction))
+  }, [query])
+  function handleSearch(e) {
+    setQuery(e.target.value)
+  }
+  return (
+    <div>
+      <Search handleSearch={handleSearch} />
+      <AddTransactionForm />
+      <TransactionsList transactions={transaction} />
+    </div>
+  );
+}
+
+export default AccountContainer;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./component/App";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+import './App.css';
+
+ReactDOM.render(<App />, document.getElementById("root"));
